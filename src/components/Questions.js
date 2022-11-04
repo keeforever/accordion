243f53ddf729a3be3135773696a836940b085776
keeforever <kaliyamoorthy.2k@gmail.com>
2022-11-04 09:04:15 +0000

    chore: maintenance

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,17 +1,24 @@
-import {useState} from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+import { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
-const Questions = ({title,info,id}) => {
-const [isOpen, setIsOpen] = useState(false)
+const Questions = ({ title, info }) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <article>
       <div className="question-container">
         <h2>{title}</h2>
-        <button className='btn-radio' onClick={()=>{setIsOpen(!isOpen)}}>{isOpen ? <AiOutlineMinus/> : <AiOutlinePlus/>}</button>
+        <button
+          className="btn-radio"
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          {isOpen ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
       </div>
       {isOpen && <p>{info}</p>}
     </article>
-  )
-}
+  );
+};
 
 export default Questions;
